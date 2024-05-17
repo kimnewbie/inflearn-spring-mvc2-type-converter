23f3ca18a32bd10118333d226dfce1adbe290372
김유진 <gracecancho@gmail.com>
2024-05-17 11:54:54 +0900

    포맷터를 지원하는 컨버전 서비스

---
+++ src/test/java/hello/typeconverter/formatter/FormattingConversionServiceTest.java
@@ -0,0 +1,37 @@
+package hello.typeconverter.formatter;
+
+import hello.typeconverter.converter.IpPortToStringConverter;
+import hello.typeconverter.converter.StringToIpPortConverter;
+import hello.typeconverter.type.IpPort;
+import org.junit.jupiter.api.Test;
+import org.springframework.format.support.DefaultFormattingConversionService;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * DefaultFormattingConversionService는
+ * FormattingConversionService 에 기본적인 통화, 숫자 관련 몇가지 기본 포맷터를 추가해서 제공한다
+ */
+public class FormattingConversionServiceTest {
+
+    @Test
+    void FormattingConversionService() {
+
+        DefaultFormattingConversionService conversionService = new DefaultFormattingConversionService();
+
+        // 컨버터 등록
+        conversionService.addConverter(new StringToIpPortConverter());
+        conversionService.addConverter(new IpPortToStringConverter());
+
+        // 포맷터 등록
+        conversionService.addFormatter(new MyNumberFormatter());
+
+        // 컨버터 사용
+        IpPort ipPort = conversionService.convert("127.0.0.1:8080", IpPort.class);
+        assertThat(ipPort).isEqualTo(new IpPort("127.0.0.1", 8080));
+
+        // 포맷터 사용
+        assertThat(conversionService.convert(1000, String.class)).isEqualTo("1,000");
+        assertThat(conversionService.convert("1,000", Long.class)).isEqualTo(1000L);
+    }
+}
